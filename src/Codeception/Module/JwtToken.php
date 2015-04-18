36d53be55736c4fa857bbc122a492d5c12bb05b6
cnam <cnam812@gmail.com>
2015-04-18 20:43:01 +0300

    initial commit

---
+++ src/Codeception/Module/JwtToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Codeception\Module;
+
+use Codeception\Module;
+use JWT;
+
+class ApiHelper extends Module
+{
+
+	protected $config = array(
+        'life_time'   => 86400,
+        'secret_key'  => 'VERY_SECRET_SIGNING_KEY',
+        'header_name' => 'X-NH-Access-Token'
+    );
+
+    /**
+     * Add header for authentificated 
+     *  
+     * @param array $payload Params for array
+     * @param string $key secret key for encode token
+     *
+     * @throws \Codeception\Exception\Module
+     */
+    public function amJWTAuthenticated($payload, $key = null)
+    {
+        $payload['exp'] = time()+$this->$config['life_time'];
+
+        if (null === $key) {
+        	$key = $this->$config['secret_key'];
+        }
+
+        $this->getModule('PhpBrowser')->setHeader($this->$config['header_name'], JWT::encode(
+            $payload,$key));
+    }
