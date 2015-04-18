50e05a39102cf764d85486f77e7e13773c1c7b07
cnam <cnam812@gmail.com>
2015-04-18 21:03:37 +0300

    fixed extension

--- src/Codeception/Module/JwtToken.php
+++ src/Codeception/Module/JwtToken.php
@@ -5,31 +5,34 @@ namespace Codeception\Module;
 use Codeception\Module;
 use JWT;
 
-class ApiHelper extends Module
+/**
+ * Class JwtToken module for usage jwt auth
+ */
+class JwtToken extends Module
 {
 
-	protected $config = array(
+    protected $config = array(
         'life_time'   => 86400,
         'secret_key'  => 'VERY_SECRET_SIGNING_KEY',
         'header_name' => 'X-NH-Access-Token'
     );
 
     /**
-     * Add header for authentificated 
-     *  
-     * @param array $payload Params for array
-     * @param string $key secret key for encode token
+     * Add header for authentificated
+     *
+     * @param array  $payload Params for array
+     * @param string $key     secret key for encode token
      *
      * @throws \Codeception\Exception\Module
      */
     public function amJWTAuthenticated($payload, $key = null)
     {
-        $payload['exp'] = time()+$this->$config['life_time'];
+        $payload['exp'] = time() + $this->config['life_time'];
 
         if (null === $key) {
-        	$key = $this->$config['secret_key'];
+            $key = $this->config['secret_key'];
         }
 
-        $this->getModule('PhpBrowser')->setHeader($this->$config['header_name'], JWT::encode(
-            $payload,$key));
+        $this->getModule('PhpBrowser')->setHeader($this->config['header_name'], JWT::encode($payload, $key));
     }
+}
